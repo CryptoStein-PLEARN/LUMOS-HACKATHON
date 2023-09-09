0ae8215294d9ec1a8d2ffdc1f6185ef8e883a499
Sanskar Tyagi <Sanskarbhartityagi@gmail.com>
2023-09-09 17:33:48 +0530

    resolving issues

--- DashBoard/src/components/radio/index.jsx
+++ DashBoard/src/components/radio/index.jsx
@@ -1,10 +1,11 @@
 const Radio = (props) => {
-  const { color, id, name, ...rest } = props;
+  const { color, id, name, checked, ...rest } = props;
   return (
     <input
       id={id}
       name={name}
       type="radio"
+      checked={checked}
       className={`before:contet[""] relative h-5 w-5 cursor-pointer appearance-none rounded-full
        border !border-gray-300 transition-all duration-[0.2s] before:absolute before:top-[3px]
        before:left-[50%] before:h-3 before:w-3 before:translate-x-[-50%] before:rounded-full before:transition-all before:duration-[0.2s] dark:!border-gray-800
--- DashBoard/src/views/admin/profile/components/General.jsx
+++ DashBoard/src/views/admin/profile/components/General.jsx
@@ -1,5 +1,6 @@
 import Card from "components/card";
-import React from "react";
+import Radio from "components/radio";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 const General = ({ data }) => {
@@ -10,50 +11,64 @@ const General = ({ data }) => {
   const handleClick = (e) => {
     history(`/admin/User/${e}`);
   };
-
+  const handleReslove = (e) => {
+    if (!e) {
+      // api call
+    }
+  };
   if (user) {
-    const userInput = user.toLowerCase(); // Convert user input to lowercase for case-insensitive comparison
-
+    const userInput = user.toLowerCase();
     const userData = data?.filter((item) =>
       item.name.toLowerCase().includes(userInput)
     );
+    const components = userData?.map(
+      (item) =>
+        !item?.resolved && (
+          <Card key={item.id} extra={"w-full h-full p-3"}>
+            <div className="mt-2 w-full ">
+              <div className="flex  items-start justify-between px-3 py-4  ">
+                <h4 className="px-2 text-xl font-bold text-navy-700 dark:text-white">
+                  User - {item.name}
+                </h4>{" "}
+                <div className="flex items-center gap-3">
+                  <span className="text-orange-400">Mark as Done</span>{" "}
+                  <Radio
+                    onClick={() => {
+                      handleReslove(item.resolved);
+                    }}
+                  ></Radio>
+                </div>
+              </div>
+              <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500  dark:!bg-navy-700 dark:shadow-none">
+                <p className="text-sm text-gray-600">Subject line</p>
+                <p className="text-base font-medium text-navy-700 dark:text-white">
+                  {item.subject}
+                </p>
+              </div>
+              <h3 className="mt-3 border-t-2 px-2 ">Message</h3>
+              <p className="  px-2 pt-2 pb-8 text-base text-gray-600">
+                {item.description}
+              </p>
+            </div>
+            {/* Cards */}
+            <div className="grid grid-cols-2 gap-4  px-2">
+              <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                <p className="text-sm text-gray-600">Email</p>
+                <p className="text-base font-medium text-navy-700 dark:text-white">
+                  {item.email}
+                </p>
+              </div>
 
-    console.log(userData);
-    const components = userData?.map((item) => (
-      <Card key={item.id} extra={"w-full h-full p-3"}>
-        <div className="mt-2 w-full ">
-          <h4 className="px-2 text-xl font-bold text-navy-700 dark:text-white">
-            User - {item.name}
-          </h4>
-          <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500  dark:!bg-navy-700 dark:shadow-none">
-            <p className="text-sm text-gray-600">Subject line</p>
-            <p className="text-base font-medium text-navy-700 dark:text-white">
-              {item.subject}
-            </p>
-          </div>
-          <h3 className="mt-3 border-t-2 px-2 ">Message</h3>
-          <p className="  px-2 pt-2 pb-8 text-base text-gray-600">
-            {item.description}
-          </p>
-        </div>
-        {/* Cards */}
-        <div className="grid grid-cols-2 gap-4  px-2">
-          <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-            <p className="text-sm text-gray-600">Email</p>
-            <p className="text-base font-medium text-navy-700 dark:text-white">
-              {item.email}
-            </p>
-          </div>
-
-          <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-            <p className="text-sm text-gray-600">Phone number</p>
-            <p className="text-base font-medium text-navy-700 dark:text-white">
-              {item.phoneNumber}
-            </p>
-          </div>
-        </div>
-      </Card>
-    ));
+              <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                <p className="text-sm text-gray-600">Phone number</p>
+                <p className="text-base font-medium text-navy-700 dark:text-white">
+                  {item.phoneNumber}
+                </p>
+              </div>
+            </div>
+          </Card>
+        )
+    );
     return (
       <div>
         <BtnBack>
