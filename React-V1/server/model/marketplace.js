95e95c07437e8d32f2c088dd26815c36fe6b7fd3
Ujjwal Sachdeva <ujjwal.sachdeva24@gmail.com>
2023-10-25 21:19:01 +0530

    Revert "update player.js"
    This reverts commit 58785c3190fb0181050c3892cd86d8553407ab91.

--- React-V1/server/model/marketplace.js
+++ React-V1/server/model/marketplace.js
@@ -41,84 +41,97 @@ const marketplaceDetail = new mongoose.model(
 const records = [
     {
         category: "characters",
+    details: [
+        {
+            id: 2,
+            name: "Steve",
+            description: "Lorem Ipsum",
+            cost: 20,
+            imgUri: "https://ik.imagekit.io/temporary/Characters/Steve.jpeg?updatedAt=1685704483948",
+            unlockLevel: 2,
+        },
+
+        {
+            id: 3,
+            name: "Bruce",
+            description: "Lorem Ipsum",
+            cost: 30,
+            imgUri: "https://ik.imagekit.io/temporary/Characters/Bruce.jpeg?updatedAt=1685704484347",
+            unlockLevel: 3,
+        },
+
+        {
+            id: 4,
+            name: "Thor",
+            description: "Lorem Ipsum",
+            cost: 40,
+            imgUri: "https://ik.imagekit.io/temporary/Characters/Thor.jpeg?updatedAt=1685704484293",
+            unlockLevel: 4,
+        },
+
+        {
+            id: 5,
+            name: "Rhodey",
+            description: "Lorem Ipsum",
+            cost: 50,
+            imgUri: "https://ik.imagekit.io/temporary/Characters/Rhodey.jpeg?updatedAt=1685704484317",
+            unlockLevel: 5,
+        },
+
+        {
+            id: 6,
+            name: "Natasha",
+            description: "Lorem Ipsum",
+            cost: 70,
+            imgUri: "https://ik.imagekit.io/temporary/Characters/Natasha.jpeg?updatedAt=1685704484706",
+            unlockLevel: 7,
+        },
+
+        {
+            id: 7,
+            name: "Wanda",
+            description: "Lorem Ipsum",
+            cost: 70,
+            imgUri: "https://ik.imagekit.io/temporary/Characters/Wanda.jpeg?updatedAt=1685704483325",
+            unlockLevel: 7,
+        },
+
+        {
+            id: 8,
+            name: "Jane",
+            description: "Lorem Ipsum",
+            cost: 70,
+            imgUri: "https://ik.imagekit.io/temporary/Characters/Jane.jpeg?updatedAt=1685704483456",
+            unlockLevel: 6,
+        },
+
+        {
+            id: 9,
+            name: "Peggy",
+            description: "Lorem Ipsum",
+            cost: 70,
+            imgUri: "https://ik.imagekit.io/temporary/Characters/Peggy.jpeg?updatedAt=1685704484891",
+            unlockLevel: 7,
+        },
+    ]
+    },
+    {
+        category: "Dice",
         details: [
             {
                 id: 2,
-                name: "Steve",
+                name: "Dice 1",
                 description: "Lorem Ipsum",
-                cost: 20,
-                imgUri: "https://ik.imagekit.io/temporary/Characters/Steve.jpeg?updatedAt=1685704483948",
-                unlockLevel: 2,
-            },
-
-            {
-                id: 3,
-                name: "Bruce",
-                description: "Lorem Ipsum",
-                cost: 30,
-                imgUri: "https://ik.imagekit.io/temporary/Characters/Bruce.jpeg?updatedAt=1685704484347",
-                unlockLevel: 3,
-            },
-
-            {
-                id: 4,
-                name: "Thor",
-                description: "Lorem Ipsum",
-                cost: 40,
-                imgUri: "https://ik.imagekit.io/temporary/Characters/Thor.jpeg?updatedAt=1685704484293",
-                unlockLevel: 4,
-            },
-
-            {
-                id: 5,
-                name: "Rhodey",
-                description: "Lorem Ipsum",
-                cost: 50,
-                imgUri: "https://ik.imagekit.io/temporary/Characters/Rhodey.jpeg?updatedAt=1685704484317",
-                unlockLevel: 5,
-            },
-
-            {
-                id: 6,
-                name: "Natasha",
-                description: "Lorem Ipsum",
-                cost: 70,
-                imgUri: "https://ik.imagekit.io/temporary/Characters/Natasha.jpeg?updatedAt=1685704484706",
-                unlockLevel: 7,
-            },
-
-            {
-                id: 7,
-                name: "Wanda",
-                description: "Lorem Ipsum",
-                cost: 70,
-                imgUri: "https://ik.imagekit.io/temporary/Characters/Wanda.jpeg?updatedAt=1685704483325",
-                unlockLevel: 7,
-            },
-
-            {
-                id: 8,
-                name: "Jane",
-                description: "Lorem Ipsum",
-                cost: 70,
-                imgUri: "https://ik.imagekit.io/temporary/Characters/Jane.jpeg?updatedAt=1685704483456",
-                unlockLevel: 6,
-            },
-
-            {
-                id: 9,
-                name: "Peggy",
-                description: "Lorem Ipsum",
-                cost: 70,
-                imgUri: "https://ik.imagekit.io/temporary/Characters/Peggy.jpeg?updatedAt=1685704484891",
-                unlockLevel: 7,
-            },
+                cost: 10,
+                imgUri: "https://ik.imagekit.io/temporary/RnB.avif?updatedAt=1685704813304",
+                unlockLevel: 1
+            }
         ]
-    },
+    }
 ]
 
 const insertOrUpdateCategory = async (category, details) => {
-    const existingRecord = await marketplaceDetail.find({ category });
+    const existingRecord = await marketplaceDetail.findOne({ category });
   
     if (existingRecord) {
       const existingDetailsIds = existingRecord.details.map((detail) => detail.id);
