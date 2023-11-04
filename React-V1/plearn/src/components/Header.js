2e26291d0b90419ab3a586a6e8efc3bf76c2202d
Beshoy Shehata <0xBeshoy@gmail.com>
2023-11-04 20:56:16 +0200

    Adding second demo play address

--- React-V1/plearn/src/components/Header.js
+++ React-V1/plearn/src/components/Header.js
@@ -4,93 +4,109 @@ import WalletContext from "../contexts/WalletContext";
 import { useContext } from "react";
 import Lottie from "lottie-react";
 import Kitty from "../assets/Gamer.json";
-import toast, {Toaster} from 'react-hot-toast'
+import toast, { Toaster } from "react-hot-toast";
 export default function Header() {
-  const { userAccount } = useContext(WalletContext);
+    const { userAccount } = useContext(WalletContext);
 
-  // demo acc
-  const startGame = async () => {
-    if (userAccount == null) {
-      alert("Please connect MetaMask wallet in order to start the game.");
-    } else {
-      window.open(
-        `https://singular-granita-0e1259.netlify.app?userAccount=${userAccount}`
-      );
-    }
-    toast.dismiss();
-  };
-const playBtn=async()=>{
-  window.open(
-    `https://singular-granita-0e1259.netlify.app?userAccount=0x0000000000000000000000000000000000000000`);
-    toast.dismiss();
-}
-const makeToast=()=>{
-  toast(
-    (t) => (
-      <span>
-          
-        Wanna try  <b>PLEARN</b>?
-        <br/> 
-        <br/> 
-        <button onClick={() => playBtn(t.id)}>Demo play</button>
-        <button onClick={() => startGame(t.id)}>Play </button>
-      </span>
-    ) 
-  );
-}
-  return (
-    <section className="hero hero2">
-      <div className="container container--relative">
-        <div className="hero__blocks">
-          <div className="hero__intro">
-            <div>
-              <h1 className="hero__intro-title animated-box mb--20">
-                <div className="block-title-inline">
-                  <img src={boxicon} alt="NFT promo" className="image" />
-                  <h4 className="">
-                    Play <span className="text-warning">|</span> Learn{" "}
-                    <span className="text-warning">|</span>
-                    Earn
-                  </h4>
-                </div>
-                <span className="gradColor display-4">PLEARN</span>
-              </h1>
-              <p className="lead fs-6 fw-light">
-                Welcome to PLEARN, the virtual board game that combines
-                education with entertainment. It provides a fun and interactive
-                way to learn financial literacy and make smart investments.
-              </p>
-              <p className="lead fs-6 fw-light">
-                Invest, buy, sell real estate, and own unique NFTs as you play
-                your way through the game.
-              </p>
-            </div>
+    // demo acc
+    const startGame = async () => {
+        if (userAccount == null) {
+            alert("Please connect MetaMask wallet in order to start the game.");
+        } else {
+            window.open(
+                `https://singular-granita-0e1259.netlify.app?userAccount=${userAccount}`
+            );
+        }
+        toast.dismiss();
+    };
+    const playBtn = async () => {
+        window.open(
+            `https://singular-granita-0e1259.netlify.app?userAccount=0xd3360559eC2D64d4276bB088AC57892f38666666`
+        );
+        toast.dismiss();
+    };
+    const makeToast = () => {
+        toast((t) => (
+            <span>
+                Wanna try <b>PLEARN</b>?
+                <br />
+                <br />
+                <button onClick={() => playBtn(t.id)}>Demo play</button>
+                <button onClick={() => startGame(t.id)}>Play </button>
+            </span>
+        ));
+    };
+    return (
+        <section className="hero hero2">
+            <div className="container container--relative">
+                <div className="hero__blocks">
+                    <div className="hero__intro">
+                        <div>
+                            <h1 className="hero__intro-title animated-box mb--20">
+                                <div className="block-title-inline">
+                                    <img
+                                        src={boxicon}
+                                        alt="NFT promo"
+                                        className="image"
+                                    />
+                                    <h4 className="">
+                                        Play{" "}
+                                        <span className="text-warning">|</span>{" "}
+                                        Learn{" "}
+                                        <span className="text-warning">|</span>
+                                        Earn
+                                    </h4>
+                                </div>
+                                <span className="gradColor display-4">
+                                    PLEARN
+                                </span>
+                            </h1>
+                            <p className="lead fs-6 fw-light">
+                                Welcome to PLEARN, the virtual board game that
+                                combines education with entertainment. It
+                                provides a fun and interactive way to learn
+                                financial literacy and make smart investments.
+                            </p>
+                            <p className="lead fs-6 fw-light">
+                                Invest, buy, sell real estate, and own unique
+                                NFTs as you play your way through the game.
+                            </p>
+                        </div>
 
-            <div className="hero__intro-cta animated-box ms-3">
-              <button className="btnply" onClick={makeToast}>
-                Play Now{" "}
-                <div id="clip">
-                  <div id="leftTop" className="corner"></div>
-                  <div id="rightBottom" className="corner"></div>
-                  <div id="rightTop" className="corner"></div>
-                  <div id="leftBottom" className="corner"></div>
+                        <div className="hero__intro-cta animated-box ms-3">
+                            <button className="btnply" onClick={makeToast}>
+                                Play Now{" "}
+                                <div id="clip">
+                                    <div id="leftTop" className="corner"></div>
+                                    <div
+                                        id="rightBottom"
+                                        className="corner"
+                                    ></div>
+                                    <div id="rightTop" className="corner"></div>
+                                    <div
+                                        id="leftBottom"
+                                        className="corner"
+                                    ></div>
+                                </div>
+                                <span id="rightArrow" className="arrow"></span>
+                                <span id="leftArrow" className="arrow"></span>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="hero__nft-placeholder-img">
+                        <Lottie className="mb-24" animationData={Kitty} />
+                    </div>
                 </div>
-                <span id="rightArrow" className="arrow"></span>
-                <span id="leftArrow" className="arrow"></span>
-              </button>
+                <Toaster
+                    toastOptions={{
+                        style: {
+                            border: "1px solid #713200",
+                            padding: "16px",
+                            color: "#713200",
+                        },
+                    }}
+                />
             </div>
-          </div>
-          <div className="hero__nft-placeholder-img">
-            <Lottie className="mb-24" animationData={Kitty} />
-          </div>
-        </div>
-        <Toaster toastOptions={{ 
-            style: {
-              border: "1px solid #713200",
-              padding: "16px",
-              color: "#713200",
-            },}} />
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
--- React-V1/plearn/src/components/Nav.js
+++ React-V1/plearn/src/components/Nav.js
@@ -6,212 +6,243 @@ import WalletContext from "../contexts/WalletContext";
 import Navbtn from "./Navbtn";
 import { useNavigate } from "react-router-dom";
 function useOutsideAlerter(ref, setX) {
-  React.useEffect(() => {
-    /**
-     * Alert if clicked on outside of element
-     */
-    function handleClickOutside(event) {
-      if (ref.current && !ref.current.contains(event.target)) {
-        setX(false);
-      }
-    }
-    // Bind the event listener
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      // Unbind the event listener on clean up
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [ref, setX]);
+    React.useEffect(() => {
+        /**
+         * Alert if clicked on outside of element
+         */
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setX(false);
+            }
+        }
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref, setX]);
 }
 
 export default function Nav() {
-  const [d,setD]=useState('none') 
-  const wrapperRef = React.useRef(null);
-  const [openWrapper, setOpenWrapper] = React.useState(false);
-  useOutsideAlerter(wrapperRef, setOpenWrapper);
+    const [d, setD] = useState("none");
+    const wrapperRef = React.useRef(null);
+    const [openWrapper, setOpenWrapper] = React.useState(false);
+    useOutsideAlerter(wrapperRef, setOpenWrapper);
 
-  // const [userAccount, setUserAccount] = useState(null);
-  const playBtn=async()=>{
-    window.open(
-      `https://singular-granita-0e1259.netlify.app?userAccount=0xd3360559ec2d64d4276bb088ac57892f38666666`);
-     
-  }
-  const [connButtonText, setConnButtonText] = useState("Connect Wallet");
-  const [isLoading, setIsLoading] = useState(false);
-  const { userAccount, setUserAccount } = useContext(WalletContext);
-  // const [ playerLevel, setPlayerLevel ] = useState(1);
-  // const [ gameCoins, setGameCoins] = useState(0);
-  const nav = useNavigate();
- const handleOpen=()=>{
-  if(d==='flex'){
-    setD('none');
-  }else{
-    setD('flex');
-  }
- }
-  const connectWalletHandler = () => {
-    if (window.ethereum && window.ethereum.isMetaMask) {
-      console.log("MetaMask Here!"); 
-      window.ethereum
-        .request({ method: "eth_requestAccounts" })
-        .then((result) => {
-          accountChangedHandler(result);
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    } else {
-      console.log("Need to install MetaMask");
-      alert("Please install MetaMask extension.");
-    }
-  };
-  const nav_list = [
-    { name: "Home" },
-    { name: "About" },
-    { name: "MarketPlace" },
-    { name: "Tokenomics" },
-    { name: "Get in Touch" },
-    // { name: "NFT" },
-  ];
-
-  const Navcomp = () => {
-    const listItems = useMemo(() => {
-      return nav_list.map((item) => {
-        const path = item.name.replace(/\s+/g, "");
-        return (
-          <>
-            <li className="nav-item" key={item.name}>
-              <div
-                className="nav-link active"
-                onClick={() => {
-                  nav(`/${item.name === "Home" ? "" : path.toLowerCase()}`);
-                }}
-              >
-                {item.name}
-              </div>
-            </li>
-          </>
+    // const [userAccount, setUserAccount] = useState(null);
+    const playBtn = async () => {
+        window.open(
+            `https://singular-granita-0e1259.netlify.app?userAccount=0xd3360559eC2D64d4276bB088AC57892f38666666`
         );
-      });
-    }, []);
-    return listItems;
-  };
+    };
+    const [connButtonText, setConnButtonText] = useState("Connect Wallet");
+    const [isLoading, setIsLoading] = useState(false);
+    const { userAccount, setUserAccount } = useContext(WalletContext);
+    // const [ playerLevel, setPlayerLevel ] = useState(1);
+    // const [ gameCoins, setGameCoins] = useState(0);
+    const nav = useNavigate();
+    const handleOpen = () => {
+        if (d === "flex") {
+            setD("none");
+        } else {
+            setD("flex");
+        }
+    };
+    const connectWalletHandler = () => {
+        if (window.ethereum && window.ethereum.isMetaMask) {
+            console.log("MetaMask Here!");
+            window.ethereum
+                .request({ method: "eth_requestAccounts" })
+                .then((result) => {
+                    accountChangedHandler(result);
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+        } else {
+            console.log("Need to install MetaMask");
+            alert("Please install MetaMask extension.");
+        }
+    };
+    const nav_list = [
+        { name: "Home" },
+        { name: "About" },
+        { name: "MarketPlace" },
+        { name: "Tokenomics" },
+        { name: "Get in Touch" },
+        // { name: "NFT" },
+    ];
 
-  // update account, will cause component re-render
-  const accountChangedHandler = (newAccount) => {
-    setUserAccount(newAccount[0]);
-    // onUserAccountChange(newAccount[0]); // Callback function - Passing the value to the parent component which is App.js
-  };
+    const Navcomp = () => {
+        const listItems = useMemo(() => {
+            return nav_list.map((item) => {
+                const path = item.name.replace(/\s+/g, "");
+                return (
+                    <>
+                        <li className="nav-item" key={item.name}>
+                            <div
+                                className="nav-link active"
+                                onClick={() => {
+                                    nav(
+                                        `/${
+                                            item.name === "Home"
+                                                ? ""
+                                                : path.toLowerCase()
+                                        }`
+                                    );
+                                }}
+                            >
+                                {item.name}
+                            </div>
+                        </li>
+                    </>
+                );
+            });
+        }, []);
+        return listItems;
+    };
 
-  useEffect(() => {
-    if (userAccount != null) {
-      setIsLoading(true);
-      axios
-        .post("https://plearn-backend.onrender.com/", { userAccount })
-        // .post("http://localhost:8080/", {userAccount})
-        .then((res) => {
-          setConnButtonText("Wallet Connected");
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-          setIsLoading(false);
-          alert("An error occurred, please try again later.");
-        });
-    } else {
-      setConnButtonText("Connect Wallet");
+    // update account, will cause component re-render
+    const accountChangedHandler = (newAccount) => {
+        setUserAccount(newAccount[0]);
+        // onUserAccountChange(newAccount[0]); // Callback function - Passing the value to the parent component which is App.js
+    };
+
+    useEffect(() => {
+        if (userAccount != null) {
+            setIsLoading(true);
+            axios
+                .post("https://plearn-backend.onrender.com/", { userAccount })
+                // .post("http://localhost:8080/", {userAccount})
+                .then((res) => {
+                    setConnButtonText("Wallet Connected");
+                    setIsLoading(false);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    setIsLoading(false);
+                    alert("An error occurred, please try again later.");
+                });
+        } else {
+            setConnButtonText("Connect Wallet");
+        }
+    }, [userAccount]);
+    const chainChangedHandler = () => {
+        window.location.reload();
+    };
+
+    if (typeof window.ethereum !== "undefined") {
+        window.ethereum.on("accountsChanged", accountChangedHandler);
+        window.ethereum.on("chainChanged", chainChangedHandler);
     }
-  }, [userAccount]);
-  const chainChangedHandler = () => {
-    window.location.reload();
-  };
 
-  if (typeof window.ethereum !== "undefined") {
-    window.ethereum.on("accountsChanged", accountChangedHandler);
-    window.ethereum.on("chainChanged", chainChangedHandler);
-  }
-
-  return (
-    <nav className="navbar navbar2 navbar-expand-lg p-0  fixed-top navbar-dark shadow-5-strong ">
-      <div className="container-fluid">
-        <div className="logo-thumbnail navbar-brand logo-custom-css">
-          {" "}
-          <img
-            src={logo_plearn}
-            onClick={() => {
-              nav("/");
-            }}
-            alt="Bootstrap"
-          />
-        </div>
-        <button
-          type="button"
-          className="navbar-toggler wltBtn"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav mb-2 mb-lg-0 me-auto">
-            <Navcomp />
-            <li
-              style={{
-                display: `${
-                  connButtonText === "Wallet Connected" ? "block" : "none"
-                }`,
-              }}
-              className="nav-item"
-            >
-              <div
-                className="nav-link"
-                onClick={() => {
-                  nav(`/owned`);
-                }}
-              >
-                Owned
-              </div>
-            </li>
-            <li className="nav-item "></li>
-          </ul>
-          <div className="flx ">
-            <Navbtn /> 
-<div class="dropdown">
-  <button ref={wrapperRef}  onMouseDown={() => setOpenWrapper(!openWrapper)}  class=" btns dropdown-toggle" type="button" id="dropdownMenuButton">
-   Play
-  </button>
-  <div style={{transform: `scale( ${openWrapper ? "1" : "0"})`}}  className={`   transition
-      `}  >
-        <div className="bss">
-
-  <div
-              className="nav-link btn  px-3 py-2  wltBtn"
-              onClick={connectWalletHandler}
-            >
-              {connButtonText}
-              {isLoading ? (
-                <span>
-                  <ColorRing
-                    visible={true}
-                    height="40"
-                    width="40"
-                    ariaLabel="blocks-loading"
-                    wrapperStyle={{}}
-                    wrapperclassName="blocks-wrapper"
-                    colors={["purple", "purple", "purple", "purple", "purple"]}
-                  />
-                </span>
-              ) : null}
-            </div> 
-    <button className="nav-link btn  px-3 py-2  wltBtn" onClick={()=>{playBtn()}}>Demo Play</button>
-    </div>
-  </div>
-</div>
-           
-           
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar navbar2 navbar-expand-lg p-0  fixed-top navbar-dark shadow-5-strong ">
+            <div className="container-fluid">
+                <div className="logo-thumbnail navbar-brand logo-custom-css">
+                    {" "}
+                    <img
+                        src={logo_plearn}
+                        onClick={() => {
+                            nav("/");
+                        }}
+                        alt="Bootstrap"
+                    />
+                </div>
+                <button
+                    type="button"
+                    className="navbar-toggler wltBtn"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarCollapse"
+                >
+                    <i className="fas fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <ul className="navbar-nav mb-2 mb-lg-0 me-auto">
+                        <Navcomp />
+                        <li
+                            style={{
+                                display: `${
+                                    connButtonText === "Wallet Connected"
+                                        ? "block"
+                                        : "none"
+                                }`,
+                            }}
+                            className="nav-item"
+                        >
+                            <div
+                                className="nav-link"
+                                onClick={() => {
+                                    nav(`/owned`);
+                                }}
+                            >
+                                Owned
+                            </div>
+                        </li>
+                        <li className="nav-item "></li>
+                    </ul>
+                    <div className="flx ">
+                        <Navbtn />
+                        <div class="dropdown">
+                            <button
+                                ref={wrapperRef}
+                                onMouseDown={() => setOpenWrapper(!openWrapper)}
+                                class=" btns dropdown-toggle"
+                                type="button"
+                                id="dropdownMenuButton"
+                            >
+                                Play
+                            </button>
+                            <div
+                                style={{
+                                    transform: `scale( ${
+                                        openWrapper ? "1" : "0"
+                                    })`,
+                                }}
+                                className={`   transition
+      `}
+                            >
+                                <div className="bss">
+                                    <div
+                                        className="nav-link btn  px-3 py-2  wltBtn"
+                                        onClick={connectWalletHandler}
+                                    >
+                                        {connButtonText}
+                                        {isLoading ? (
+                                            <span>
+                                                <ColorRing
+                                                    visible={true}
+                                                    height="40"
+                                                    width="40"
+                                                    ariaLabel="blocks-loading"
+                                                    wrapperStyle={{}}
+                                                    wrapperclassName="blocks-wrapper"
+                                                    colors={[
+                                                        "purple",
+                                                        "purple",
+                                                        "purple",
+                                                        "purple",
+                                                        "purple",
+                                                    ]}
+                                                />
+                                            </span>
+                                        ) : null}
+                                    </div>
+                                    <button
+                                        className="nav-link btn  px-3 py-2  wltBtn"
+                                        onClick={() => {
+                                            playBtn();
+                                        }}
+                                    >
+                                        Demo Play
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
